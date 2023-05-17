c09812605252caf55ba4547422bf213a8972f36f
Marcus Vinícius Costa <marcus@mvscti.com.br>
2023-05-17 09:39:09 -0300

    Atualizando ProxyPatternDemo.java

--- padroes/estruturais/proxy/ProxyPatternDemo.java
+++ padroes/estruturais/proxy/ProxyPatternDemo.java
@@ -4,7 +4,6 @@ public class ProxyPatternDemo {
     public static void main(String[] args){
         String filename="image.jpg";
         Image image = new ProxyImage(filename);  
-        image.display();  
-        image.display();  
+        image.display();
     }
 }
