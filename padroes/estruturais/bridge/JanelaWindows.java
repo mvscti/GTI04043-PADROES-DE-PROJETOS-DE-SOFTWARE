466771ade96e1887487d6dd8f237fe17ed6ac498
marcus@mvscti.com.br <>
2023-05-22 09:47:58 -0300

    Adicionando padrão bridge

---
+++ padroes/estruturais/bridge/JanelaWindows.java
@@ -0,0 +1,15 @@
+package padroes.estruturais.bridge;
+
+public class JanelaWindows implements Janela {
+ 
+    @Override
+    public void desenharJanela(String titulo) {
+        System.out.println(titulo + " - Janela Windows");
+    }
+ 
+    @Override
+    public void desenharBotao(String titulo) {
+        System.out.println(titulo + " - Botão Windows");
+    }
+ 
+}
